23fead04c037b66b429f30843075c19a26563886
Tobyha01 <tobyhamilton01@gmail.com>
2023-02-24 22:55:13 +0000

    Created an express web server which generates a HTML page located in a 'public' folder

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+const express = require("express")
+const app = express()
+const port = 3000
 
+app.use(express.static("public"))
+app.get("/", function (request, respone){
+
+})
+app.listen(port, function() {
+    console.log(`Server listening at https://localhost:${port}`)
+})
